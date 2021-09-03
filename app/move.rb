72823c502f0229f58611cfc7b4805b40fe170790
djdefi <djdefi@gmail.com>
2021-09-03 22:16:40 +0000

    custom snek

--- app/move.rb
+++ app/move.rb
@@ -3,7 +3,7 @@
 # TODO: Use the information in board to decide your next move.
 def move(board)
   puts board
-
+  
   # Choose a random direction to move in
   possible_moves = ["up", "down", "left", "right"]
   move = possible_moves.sample

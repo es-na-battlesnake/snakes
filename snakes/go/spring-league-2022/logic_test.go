cf4a87271cdf6cf6e925258fcbc8af7d3904271e
Brian Evans <bevns@github.com>
2022-02-04 00:31:18 +0000

    adding more tests

--- snakes/go/spring-league-2022/logic_test.go
+++ snakes/go/spring-league-2022/logic_test.go
@@ -18,8 +18,8 @@ func TestNeckAvoidance(t *testing.T) {
 		You: me,
 	}
 
-	// Act 1,000x (this isn't a great way to test, but it's okay for starting out)
-	for i := 0; i < 100; i++ {
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
 		nextMove := move(state)
 		// Assert never move left
 		if nextMove.Move == "left" {
@@ -43,8 +43,8 @@ func TestBodyAvoidance(t *testing.T) {
 		You: me,
 	}
 
-	// Act 1,000x (this isn't a great way to test, but it's okay for starting out)
-	for i := 0; i < 100; i++ {
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
 		nextMove := move(state)
 		// Assert never move right
 		if nextMove.Move == "right" {
@@ -57,9 +57,9 @@ func TestBodyAvoidance(t *testing.T) {
 func TestFoodEating(t *testing.T) {
 	// Arrange
 	me := Battlesnake{
-		// Length 4
-		Head: Coord{X: 2, Y: 1},
-		Body: []Coord{{X: 2, Y: 1}, {X: 2, Y: 0}, {X: 3, Y: 0}, {X: 4, Y: 0}},
+		Head:   Coord{X: 2, Y: 1},
+		Body:   []Coord{{X: 2, Y: 1}, {X: 2, Y: 0}, {X: 3, Y: 0}, {X: 4, Y: 0}},
+		Health: 20,
 	}
 	state := GameState{
 		Board: Board{
@@ -68,8 +68,8 @@ func TestFoodEating(t *testing.T) {
 		},
 		You: me,
 	}
-	// Act 1,000x (this isn't a great way to test, but it's okay for starting out)
-	for i := 0; i < 5; i++ {
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
 		nextMove := move(state)
 		// Assert never move right
 		if nextMove.Move != "left" {
@@ -82,12 +82,11 @@ func TestFoodEating(t *testing.T) {
 func TestFoodEating2(t *testing.T) {
 	// Arrange
 	me := Battlesnake{
-		// Length 4
-		Head: Coord{X: 2, Y: 1},
-		Body: []Coord{{X: 2, Y: 1}, {X: 2, Y: 0}, {X: 3, Y: 0}, {X: 4, Y: 0}},
+		Head:   Coord{X: 2, Y: 1},
+		Body:   []Coord{{X: 2, Y: 1}, {X: 2, Y: 0}, {X: 3, Y: 0}, {X: 4, Y: 0}},
+		Health: 20,
 	}
 	other := Battlesnake{
-		// Length 4
 		Head: Coord{X: 1, Y: 1},
 		Body: []Coord{{X: 1, Y: 2}, {X: 1, Y: 3}, {X: 1, Y: 4}, {X: 1, Y: 5}},
 	}
@@ -98,8 +97,8 @@ func TestFoodEating2(t *testing.T) {
 		},
 		You: me,
 	}
-	// Act 1,000x (this isn't a great way to test, but it's okay for starting out)
-	for i := 0; i < 5; i++ {
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
 		nextMove := move(state)
 		// Assert never move right
 		if nextMove.Move == "left" {
@@ -108,4 +107,91 @@ func TestFoodEating2(t *testing.T) {
 	}
 }
 
-// TODO: More GameState test cases!
+// Test that we do not wrap around into our own body.
+func TestBodyWrap(t *testing.T) {
+	// Arrange
+	me := Battlesnake{
+		Head: Coord{X: 10, Y: 5},
+		Body: []Coord{{X: 10, Y: 5}, {X: 9, Y: 5}, {X: 8, Y: 5}, {X: 0, Y: 5}},
+	}
+	state := GameState{
+		Board: Board{
+			Snakes: []Battlesnake{me},
+			Height: 11,
+			Width:  11,
+		},
+		You: me,
+		Game: Game{
+			Ruleset: Ruleset{
+				Name: "wrapped",
+			},
+		},
+	}
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
+		nextMove := move(state)
+		// Assert never move right
+		if nextMove.Move == "right" {
+			t.Errorf("snake wrapped into its own body, %s", nextMove.Move)
+		}
+	}
+}
+
+// Test that we do not wrap around the board into another snake.
+func TestBodyWrap2(t *testing.T) {
+	// Arrange
+	me := Battlesnake{
+		Head: Coord{X: 10, Y: 5},
+		Body: []Coord{{X: 10, Y: 5}, {X: 9, Y: 5}, {X: 8, Y: 5}, {X: 7, Y: 5}},
+	}
+	other := Battlesnake{
+		Head: Coord{X: 0, Y: 5},
+		Body: []Coord{{X: 0, Y: 5}, {X: 0, Y: 6}, {X: 0, Y: 7}, {X: 0, Y: 8}},
+	}
+	state := GameState{
+		Board: Board{
+			Snakes: []Battlesnake{me, other},
+			Height: 11,
+			Width:  11,
+		},
+		You: me,
+		Game: Game{
+			Ruleset: Ruleset{
+				Name: "wrapped",
+			},
+		},
+	}
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
+		nextMove := move(state)
+		// Assert never move right
+		if nextMove.Move == "right" {
+			t.Errorf("snake wrapped into another snake, %s", nextMove.Move)
+		}
+	}
+}
+
+// Test that we avoid walls when not in wrapped mode.
+func TestWallAvoidance(t *testing.T) {
+	// Arrange
+	me := Battlesnake{
+		Head: Coord{X: 10, Y: 5},
+		Body: []Coord{{X: 10, Y: 5}, {X: 9, Y: 5}, {X: 8, Y: 5}, {X: 7, Y: 5}},
+	}
+	state := GameState{
+		Board: Board{
+			Snakes: []Battlesnake{me},
+			Height: 11,
+			Width:  11,
+		},
+		You: me,
+	}
+	// Act 1000x (this isn't a great way to test, but it's okay for starting out)
+	for i := 0; i < 1000; i++ {
+		nextMove := move(state)
+		// Assert never move right
+		if nextMove.Move == "right" {
+			t.Errorf("snake moved into wall, %s", nextMove.Move)
+		}
+	}
+}

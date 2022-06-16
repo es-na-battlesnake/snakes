2519e30a55481e11f6a59515df156bc92d0f3135
Brian Evans <bevns@github.com>
2022-06-16 00:19:40 +0000

    comment out old functions

--- snakes/python/summer-league-2022/src/logic.py
+++ snakes/python/summer-league-2022/src/logic.py
@@ -145,55 +145,10 @@ def choose_move(data: dict) -> str:
 
     return get_direction(path)
 
-
-    """
-    !!!!!!!!! 
-    OLD MOVE CODE BELOW
-    !!!!!!!!!
-    """
-
-    my_snake = data["you"]      # A dictionary describing your snake's position on the board
-    my_head = my_snake["head"]  # A dictionary of coordinates like {"x": 0, "y": 0}
-    my_body = my_snake["body"]  # A list of coordinate dictionaries like [{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}]
-    
-    #create an array of arrays to store other snakes' body
-    other_snakes = _get_other_snakes(data)
-
-    # Uncomment the lines below to see what this data looks like in your output!
-    # print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
-    # print(f"All board data this turn: {data}")
-    # print(f"My Battlesnake this turn is: {my_snake}")
-    # print(f"My Battlesnakes head this turn is: {my_head}")
-    # print(f"My Battlesnakes body this turn is: {my_body}")
-
-    possible_moves = ["up", "down", "left", "right"]
-
-    # Step 0: Don't allow your Battlesnake to move back on it's own neck.
-    possible_moves = _avoid_my_neck(my_body, possible_moves)
-
-    # TODO: Step 1 - Don't hit walls.
-    # Use information from `data` and `my_head` to not move beyond the game board.
-    board = data["board"]
-    possible_moves = _avoid_walls(my_body, possible_moves, board)
-
-    # Use information from `my_body` to avoid moves that would collide with yourself.
-    possible_moves = _avoid_my_body(my_body, possible_moves)
-
-    # Step 3 - Don't collide with others.
-    possible_moves = _avoid_snake(my_body, other_snakes, possible_moves)
-
-    # TODO: Step 4 - Find food.
-    # Use information in `data` to seek out and find food.
-    # food = data['board']['food']
-
-    # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    move = random.choice(possible_moves)
-    # TODO: Explore new strategies for picking a move that are better than random
-
-    print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
-
-    return move
-
+'''
+The functions below this point are only used by the tests. 
+They can be removed and tests can be cleaned up.
+'''
 
 def _avoid_my_neck(my_body: dict, possible_moves: List[str]) -> List[str]:
     """
